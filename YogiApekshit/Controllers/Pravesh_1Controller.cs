73c9f1f831ce91064e6a7ec4b7bd556951137cfc
Vyom131313 <vyom131313@yahoo.com>
2016-03-24 20:57:41 -0400

    a

--- YogiApekshit/Controllers/Pravesh_1Controller.cs
+++ YogiApekshit/Controllers/Pravesh_1Controller.cs
@@ -4,12 +4,24 @@ namespace YogiApekshit.Controllers
 {
     public class Pravesh_1Controller : BaseController
     {
-        //
-        // GET: /Pravesh-1/
         public ActionResult Que_1()
         {
             return View();
         }
 
+        public ActionResult Que_2()
+        {
+            return View();
+        }
+
+        public ActionResult Que_3()
+        {
+            return View();
+        }
+
+        public ActionResult Que_4()
+        {
+            return View();
+        }
     }
 }
