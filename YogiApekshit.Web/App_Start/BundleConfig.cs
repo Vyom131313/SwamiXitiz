3c313078f1d9a4d3bc03d4c8a6cfab163276f905
vyom131313 <vyom131313@yahoo.com>
2016-05-22 21:19:54 -0400

    a

---
+++ YogiApekshit.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace YogiApekshit.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/AwesomeAngularMVCApp")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .IncludeDirectory("~/Scripts/Factories", "*.js")
+                .Include("~/Scripts/AwesomeAngularMVCApp.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = false;
+        }
+    }
+}
