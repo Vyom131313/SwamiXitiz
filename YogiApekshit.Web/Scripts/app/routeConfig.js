38c39b2f236293ede02bb326ba8fa285291cbfe0
vyom131313 <vyom131313@yahoo.com>
2016-05-30 22:43:58 -0400

    a

--- YogiApekshit.Web/Scripts/app/routeConfig.js
+++ YogiApekshit.Web/Scripts/app/routeConfig.js
@@ -1,18 +1,15 @@
 ﻿angular
-    .module('MyApp', [
-    'ngRoute',
-    'MyApp.ctrl.crud',
-])
-.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    .module('MyApp', ['ngRoute', 'MyApp.ctrl.crud'])
+    .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
-    $routeProvider.when('/', {
-        templateUrl: '/Home/CRUD',
-        controller: 'loginController'
-    });
+        $routeProvider.when('/', {
+            templateUrl: '/Home/CRUD',
+            controller: 'loginController'
+        });
 
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
-        
-}]);
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+    }]);
