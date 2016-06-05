12800c91c1eda02eb94683ea25d03a8b6e71ce1b
vyom131313 <vyom131313@yahoo.com>
2016-06-04 23:20:57 -0400

    a

--- YogiApekshit.Web/Scripts/Controllers/PrarambhController.js
+++ YogiApekshit.Web/Scripts/Controllers/PrarambhController.js
@@ -6,68 +6,20 @@
             $scope.Message = "Yahoooo!.";
 
             /************** Que_1 *****************/
-            $scope.Que_1_List = [];
-            $scope.Que_1_List_Load = function () {
+            $scope.QA_List = [];
+            $scope.QA_List_Load = function (category) {
                 $.ajax({
                     type: 'GET',
                     contentType: 'application/json; charset=utf-8',
-                    data: { category: "WhoWhomWhen" },
+                    //data: { category: "WhoWhomWhen" },
+                    data: { category: category },
                     url: '/Prarambh/QA_List',
                     success: function (data) {
-                        $scope.Que_1_List = data
+                        $scope.QA_List = data
                         $scope.$apply();
                     }
                 });
             }
-            $scope.Que_1_List_Load();
-
-            /************** Que_2 *****************/
-            $scope.Que_2_List = [];
-            $scope.Que_2_List_Load = function () {
-                $.ajax({
-                    type: 'GET',
-                    contentType: 'application/json; charset=utf-8',
-                    data: { category: "OneSentence" },
-                    url: '/Prarambh/QA_List',
-                    success: function (data) {
-                        $scope.Que_2_List = data
-                        $scope.$apply();
-                    }
-                });
-            }
-            $scope.Que_2_List_Load();
-
-            /************** Que_3 *****************/
-            $scope.Que_3_List = [];
-            $scope.Que_3_List_Load = function () {
-                $.ajax({
-                    type: 'GET',
-                    contentType: 'application/json; charset=utf-8',
-                    data: { category: "CorrectSentence" },
-                    url: '/Prarambh/QA_List',
-                    success: function (data) {
-                        $scope.Que_3_List = data
-                        $scope.$apply();
-                    }
-                });
-            }
-            $scope.Que_3_List_Load();
-
-            /************** Que_3 *****************/
-            $scope.Que_4_List = [];
-            $scope.Que_4_List_Load = function () {
-                $.ajax({
-                    type: 'GET',
-                    contentType: 'application/json; charset=utf-8',
-                    data: { category: "ShortNote" },
-                    url: '/Prarambh/QA_List',
-                    success: function (data) {
-                        $scope.Que_4_List = data
-                        $scope.$apply();
-                    }
-                });
-            }
-            $scope.Que_4_List_Load();
         }
         ]);
 })();
